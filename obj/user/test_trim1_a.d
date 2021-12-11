be3096ff35871290e14637d53a825307d4034da5
HodBossHod <hadiehab17@gmail.com>
2021-12-11 16:20:34 +0200

    Test

---
+++ obj/user/test_trim1_a.d
@@ -0,0 +1,4 @@
+obj/user/test_trim1_a.o: user/test_trim1_a.c inc/lib.h inc/types.h \
+ inc/stdio.h inc/stdarg.h inc/string.h inc/error.h inc/assert.h \
+ inc/environment_definitions.h inc/queue.h inc/trap.h inc/memlayout.h \
+ inc/mmu.h inc/syscall.h inc/uheap.h inc/ubuddy.h
---
+++ obj/user/tst_page_replacement_mod_clock.d
@@ -0,0 +1,5 @@
+obj/user/tst_page_replacement_mod_clock.o: \
+ user/tst_page_replacement_mod_clock.c inc/lib.h inc/types.h inc/stdio.h \
+ inc/stdarg.h inc/string.h inc/error.h inc/assert.h \
+ inc/environment_definitions.h inc/queue.h inc/trap.h inc/memlayout.h \
+ inc/mmu.h inc/syscall.h inc/uheap.h inc/ubuddy.h
